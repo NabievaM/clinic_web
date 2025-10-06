abc410a60966982d3d0e076850ab4893a0c030b3
Mukhlis Nabieva <nabievamuhlisa24@gmail.com>
2025-10-06 14:40:41 +0500

    added production domain to CORS origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173', 
+      'https://topmadad.uz', 
+    ],
     credentials: true,
   });
 
